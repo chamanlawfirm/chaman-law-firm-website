f6da3e29cce2582cab5b266cba5b826f6f4fb600
Charles Chukwuma Nkwoka <chamanlawfirm@gmail.com>
2026-05-30 21:38:13 +0100

    Register Sanity external studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,5 +6,9 @@ import { defineCliConfig } from 'sanity/cli'
 
 const projectId = 'eeuefmhu'
 const dataset = 'production'
+const appId = 'ut35nzr86hplimqdq9ai4kzw'
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({
+  api: {projectId, dataset},
+  deployment: {appId},
+})
